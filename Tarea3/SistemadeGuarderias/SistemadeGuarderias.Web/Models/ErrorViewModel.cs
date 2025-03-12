2ce0fca3a73860a43b873f58838feeeb746d5171
angerypr <angerypr16@gmail.com>
2025-03-12 16:48:51 -0400

    Creacion proyecto Web

---
+++ Tarea3/SistemadeGuarderias/SistemadeGuarderias.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemadeGuarderias.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
